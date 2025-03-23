f64787f8c72ee511e0a814d60eaeab50c200b3d5
Shikhar-blockchain@gmail.com <shikharshukla5525678@gmail.com>
2025-03-23 14:04:14 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,9 +2,36 @@ const express = require("express");
 
 const app = express();
 
-app.get(/^\/data\/[0-9]+$/, (req, res) => {
-  res.send("this is working");
-});
+// app.use("/user", [rout-1,rout-2,[rout-3,rout-4],rout-5,rout-ect]);
+
+app.use("/user", [
+  (req, res, next) => {
+    console.log("Handiling rout usage 1!");
+    // res.send("1st Response");
+    next();
+  },
+  [
+    (req, res, next) => {
+      console.log("Handiling rout usage 2!");
+      // res.send("2nd Response");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Handiling rout usage 3!");
+      // res.send("2nd Response");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Handiling rout usage 4!");
+      // res.send("2nd Response");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    console.log("Handiling rout usage 5!");
+    res.send("5th Response");
+  },
+]);
 
 app.listen(1111, () => {
   console.log("Surver is succesufally listening to 1111");
