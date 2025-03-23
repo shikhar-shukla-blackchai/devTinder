518a6e722808ecd00c0b579cdcf76d643c5ce03b
Shikhar-blockchain@gmail.com <shikharshukla5525678@gmail.com>
2025-03-23 12:38:48 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,25 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ firstName: "shikhar", lastNmae: "shukla" });
-});
-
-app.post("/user", (req, res) => {
-  // send data to da
-  res.send("Data successfully save to the dataBase");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully");
-});
-
-app.patch("/user", (req, res) => {
-  res.send("Data updated succesfully in the data base");
-});
-
-app.use("/test", (req, res) => {
-  console.log("Hellow form the server");
+app.get(/^\/data\/[0-9]+$/, (req, res) => {
+  res.send("this is working");
 });
 
 app.listen(1111, () => {
