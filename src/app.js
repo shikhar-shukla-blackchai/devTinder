3bd430a06ea71e0ed23d35180172a8c329e680c6
Shikhar-blockchain@gmail.com <shikharshukla5525678@gmail.com>
2025-03-23 10:52:25 +0530

    exploring get, post, patch, delete

--- src/app.js
+++ src/app.js
@@ -2,15 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Ha! it works?");
-});
-app.get("/jara", (req, res) => {
-  res.send("Ja ra app.use nahi caleja");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "shikhar", lastNmae: "shukla" });
 });
 
-app.get("/home", (req, res) => {
-  res.send("Home path is also working");
+app.post("/user", (req, res) => {
+  // send data to da
+  res.send("Data successfully save to the dataBase");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("Data updated succesfully in the data base");
+});
+
+app.use("/test", (req, res) => {
+  console.log("Hellow form the server");
 });
 
 app.listen(1111, () => {
